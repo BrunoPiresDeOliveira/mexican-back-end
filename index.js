1fa9655549ddbb577fc7e8e9b251813ce0ab93ca
Bruno <bruno.pires@aluno.faculdadeimpacta.com>
2021-06-15 19:46:05 -0300

    up

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const customExpress = require('./src/config/customExpress')
 
 const app = customExpress()
 
-app.listen(process.env.SERVER_PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Server listening at http://localhost:${process.env.SERVER_PORT}`)
 })
